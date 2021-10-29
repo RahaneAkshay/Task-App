7cf08e0c09dae35c9be3fcf64fc2d311a32edeeb
Akshay <akshay29610@gmail.com>
2021-10-29 11:31:04 +0530

    Auth middleware added

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,7 +1,8 @@
 import { Request, Router, Response } from "express";
 import { fetchTasks ,createTask, updateTask,deleteTask } from "../controllers/task.controller";
+
 export const task = Router();
-task.get("/fetch", fetchTasks);
+task.get("/fetch",fetchTasks);
 
 task.post("/create", createTask);
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import {createUser,readUser,updateUser,deleteUser,loginUser} from '../controllers/user.controller'
+import { auth } from "../middleware/auth.middleware";
 export const user = Router();
 
-user.get('/fetch',readUser)
+user.get('/fetch',auth,readUser)
 user.post('/create',createUser)
 user.post('/login',loginUser)
-user.patch('/update/:id',updateUser)
-user.delete('/delete/:id',deleteUser)
+user.patch('/update/:id',auth,updateUser)
+user.delete('/delete/:id',auth,deleteUser)
